56aa1623795292f1e596085effdeb319b2865a1d
Romina <rominaabadi1994@gmail.com>
2021-03-10 20:36:44 -0500

    Nonzero gradients :), still not working :(

--- JAXGAN.py
+++ JAXGAN.py
@@ -1,6 +1,6 @@
 import jax.numpy as jnp
 import numpy as np
-from jax import grad, jit, vmap
+from jax import grad, jit, vmap, value_and_grad
 from jax import random
 from functools import partial
 from jax.scipy.special import logsumexp
@@ -48,8 +48,20 @@ class FlattenAndCast(object):
         return np.ravel(np.array(pic, dtype=jnp.float32))
 
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~ Helper Functions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def one_hot(x, k, dtype=jnp.float32):
+    """Create a one-hot encoding of x of size k."""
+    return jnp.array(x[:, None] == jnp.arange(k), dtype)
+
+
+def accuracy(params, images, targets):
+    target_class = jnp.argmax(targets, axis=1)
+    predicted_class = jnp.argmax(batched_disc_predict(params, images), axis=1)
+    return jnp.mean(predicted_class == target_class)
+
+
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~ START OF GAN ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-def random_layer_params(m, n, key, scale=1e-2):
+def random_layer_params(m, n, key, scale=2e-2):
     w_key, b_key = random.split(key)
     return scale * random.normal(w_key, (n, m)), scale * random.normal(b_key, (n,))
 
@@ -60,13 +72,14 @@ def init_network_params(sizes, key):
 
 
 dist_dim = 100
-disc_layer_sizes = [784, 512, 256, 2]
-gen_layer_sizes = [dist_dim, 256, 512, 784]
+d_layer_sizes = [784, 512, 256, 2]
+g_layer_sizes = [dist_dim, 256, 512, 784]
 # param_scale = 0.1
-step_size = 0.01
-num_epochs = 1
+d_step_size = 0.0002
+g_step_size = 0.0002
+num_epochs = 10
 batch_size = 128
-n_targets = 10
+# n_targets = 10
 
 
 def relu(x):
@@ -105,93 +118,99 @@ def gen_generate(params, noise):
 batched_gen_generate = vmap(gen_generate, in_axes=(None, 0), out_axes=0)
 
 
-def one_hot(x, k, dtype=jnp.float32):
-    """Create a one-hot encoding of x of size k."""
-    return jnp.array(x[:, None] == jnp.arange(k), dtype)
-
-
-def accuracy(params, images, targets):
-    target_class = jnp.argmax(targets, axis=1)
-    predicted_class = jnp.argmax(batched_disc_predict(params, images), axis=1)
-    return jnp.mean(predicted_class == target_class)
-
-
 def disc_loss(d_params, images, targets):
     preds = batched_disc_predict(d_params, images)
     # return -jnp.mean(preds * targets)
-    return jnp.mean(preds * targets)
+    return -jnp.mean(preds * targets)
 
 
 # @jit
 def update_disc(d_params, images, labels):
-    grads = grad(disc_loss)(d_params, images, labels)
-    return [(w - step_size * dw, b - step_size * db)
-            for (w, b), (dw, db) in zip(d_params, grads)], grads
+    """
+
+    :param g_params:
+    :param d_params:
+    :param g_noise:
+    :return: params, loss value, grads
+    """
+    dics_loss_value, d_grads = value_and_grad(disc_loss)(d_params, images, labels)
+    return [(w - d_step_size * dw, b - d_step_size * db)
+            for (w, b), (dw, db) in zip(d_params, d_grads)], dics_loss_value, d_grads
 
 
 def gen_loss(g_params, d_params, g_noise):
     ims = batched_gen_generate(g_params, g_noise)
     # plt.imshow(jnp.reshape(ims[0], (28, 28)))
     # plt.show()
-    preds = batched_disc_predict(d_params, ims)
-    return -jnp.mean(preds * one_hot(jnp.ones(len(preds)), 1))
+    return disc_loss(d_params, ims, one_hot(jnp.ones(len(ims)), 2))
     # return -jnp.mean(preds * one_hot(jnp.zeros(len(preds)), 1))
 
 
 # @jit
-def update_gen(g_params, d_params, gen_noise):
-    grads = grad(gen_loss)(g_params, d_params, gen_noise)
+def update_gen(g_params, d_params, g_noise):
+    """
 
-    return [(w + step_size * dw, b + step_size * db)
-            for (w, b), (dw, db) in zip(g_params, grads)], grads
+    :param g_params:
+    :param d_params:
+    :param g_noise:
+    :return: params, loss value, grads
+    """
+    g_loss_value, g_grads = value_and_grad(gen_loss)(g_params, d_params, g_noise)
+
+    return [(w + g_step_size * dw, b + g_step_size * db)
+            for (w, b), (dw, db) in zip(g_params, g_grads)], g_loss_value, g_grads
 
 
+# ~~~~~~~~~~~~~~~~~~~~~~~ Data Loader, I don't understand exactly ~~~~~~~~~~~~~~~~~~~~``
 mnist_dataset = MNIST('./tmp/mnist/', download=True, transform=FlattenAndCast())
 # load training with the generator (makes batch easier I think)
 training_generator = NumpyLoader(mnist_dataset, batch_size=batch_size, num_workers=0)
 train_images = np.array(mnist_dataset.train_data).reshape(len(mnist_dataset.train_data), -1)
-train_labels = one_hot(np.array(mnist_dataset.train_labels), n_targets)
+# train_labels = one_hot(np.array(mnist_dataset.train_labels), n_targets)
 # Get full test dataset
 # mnist_dataset_test = MNIST('./tmp/mnist/', download=True, train=False)
 # test_images = jnp.array(mnist_dataset_test.test_data.numpy().reshape(len(mnist_dataset_test.test_data), -1),
 #                         dtype=jnp.float32)
 # test_labels = one_hot(np.array(mnist_dataset_test.test_labels), n_targets)
 
-disc_params = init_network_params(disc_layer_sizes, random.PRNGKey(0))
-gen_params = init_network_params(gen_layer_sizes, random.PRNGKey(0))
+
+key = random.PRNGKey(0)
+key, dkey, gkey = random.split(key, 3)
+d_params = init_network_params(d_layer_sizes, dkey)
+g_params = init_network_params(g_layer_sizes, gkey)
 for epoch in range(num_epochs):
     start_time = time.time()
-    key = random.PRNGKey(0)
-    key, subkey = random.split(key)
     for real_images, _ in training_generator:
-        noise = random.normal(key, (batch_size, dist_dim), dtype=jnp.float32)
-        key, subkey = random.split(subkey)
-        fake_images = batched_gen_generate(gen_params, noise)
-        disc_t_imgs = jnp.concatenate([real_images, fake_images])
-        disc_t_lbls = jnp.concatenate([one_hot(jnp.ones(len(real_images)), 2),
-                                       one_hot(jnp.zeros(len(fake_images)), 2)])
-        # disc_t_lbls = disc_t_lbls + 0.05 * random.uniform(key, disc_t_lbls.shape)
-        # key, subkey = random.split(subkey)
-        disc_t_lbls = 0.9 * disc_t_lbls
-        # print(f'disc weight before update:{gen_params[0][0][0:3,0:1]}')
-        disc_params, gradd = update_disc(disc_params, disc_t_imgs, disc_t_lbls)
-        # print(f'disc weight after update:{gen_params[0][0][0:3,0:1]}')
-        print(f'disc grad after update:{gradd[0][0][0:3, 0:1]}')
-        noise = random.normal(key, (batch_size, dist_dim), dtype=jnp.float32)
-        key, subkey = random.split(subkey)
-        # print(f'gen weight before update:{gen_params[0][0][0:3,0:1]}')
-        gen_params, gradg = update_gen(gen_params, disc_params, noise)
-        # print(f'gen weight after update:{gen_params[0][0][0:3,0:1]}')
-        print(f'gen grad after update:{gradg[0][0][0:3,0:1]}')
+        plt.imshow(real_images[0])
+        plt.show()
+        key, subkey = random.split(key)
+        noise = random.normal(subkey, (batch_size, dist_dim), dtype=jnp.float32)
+        fake_images = batched_gen_generate(g_params, noise)
+
+        d_t_imgs = jnp.concatenate([real_images, fake_images])
+        d_t_lbls = jnp.concatenate([one_hot(jnp.ones(len(real_images)), 2),
+                                    one_hot(jnp.zeros(len(fake_images)), 2)])
+
+        d_t_lbls = 0.9 * d_t_lbls
+        d_params, d_loss, d_grad = update_disc(d_params, d_t_imgs, d_t_lbls)
+        print(f'disc grad after update:{d_grad[0][0][0:3, 0:1]}')
+        print(f'disc loss:{d_loss}')
+
+        # TODO: to create new noise or not to create new noise?
+        # key, subkey = random.split(key)
+        # noise = random.normal(subkey, (batch_size, dist_dim), dtype=jnp.float32)
+
+        g_params, g_loss, g_grad = update_gen(g_params, d_params, noise)
+        print(f'gen grad after update:{g_grad[0][0][0:3,0:1]}')
+        print(f'gen loss:{g_loss}')
+
     epoch_time = time.time() - start_time
 
-    train_acc = accuracy(disc_params, train_images, one_hot(np.ones(len(train_images)), 2))
-    # test_acc = accuracy(disc_params, test_images, test_labels)
     print("Epoch {} in {:0.2f} sec".format(epoch, epoch_time))
-    print("Training set accuracy {}".format(train_acc))
-    # print("Test set accuracy {}".format(test_acc))
-    noise = random.normal(key, (1, dist_dim), dtype=jnp.float32)
-    key, subkey = random.split(subkey)
-    fake_image = batched_gen_generate(gen_params, noise)
+
+    key, subkey = random.split(key)
+    noise = random.normal(subkey, (1, dist_dim), dtype=jnp.float32)
+
+    fake_image = batched_gen_generate(g_params, noise)
     plt.imshow(jnp.reshape(fake_image, (28, 28)))
     plt.show()
