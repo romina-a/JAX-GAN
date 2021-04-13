f860e9bdbab97be961a3918f0fa23c241b31d577
Romina <rominaabadi1994@gmail.com>
2021-04-13 15:15:31 -0400

    adjustments

--- StaxGAN.py
+++ StaxGAN.py
@@ -70,9 +70,12 @@ g_momentum_default = 0.5
 g_momentum2_default = 0.5
 loss_function_default = BCE_from_logits
 
+random_seed_default = 0
+
 batch_size_default = 256
 num_iter_default = 2000
 digit_default = 0
+
 
 
 def conv_generator():
@@ -114,17 +117,8 @@ def conv_discriminator():
     return model
 
 
-d_init, d_apply = conv_discriminator()
-g_init, g_apply = conv_generator()
-
-(d_opt_init_fun, d_opt_update_fun, d_opt_get_params) = adam(d_lr_default, d_momentum_default, d_momentum2_default)
-(g_opt_init_fun, g_opt_update_fun, g_opt_get_params) = adam(g_lr_default, g_momentum_default, g_momentum2_default)
-d_opt = {'init': d_opt_init_fun, 'update': d_opt_update_fun, 'get_params': d_opt_get_params}
-g_opt = {'init': g_opt_init_fun, 'update': g_opt_update_fun, 'get_params': g_opt_get_params}
-
-
 @partial(jit, static_argnums=(3,))
-def d_loss(d_params, g_params, prng_key, batch_size, real_ims):
+def d_loss(d_params, g_params, d_apply, g_apply, prng_key, batch_size, real_ims):
     z = jax.random.normal(prng_key, (batch_size, 100))
     fake_ims = g_apply(g_params, z)
 
@@ -138,7 +132,7 @@ def d_loss(d_params, g_params, prng_key, batch_size, real_ims):
 
 
 @partial(jit, static_argnums=(3,))
-def g_loss(g_params, d_params, prng_key, batch_size):
+def g_loss(g_params, d_params, g_apply, d_apply, prng_key, batch_size):
     z = jax.random.normal(prng_key, (batch_size, 100))
     fake_ims = g_apply(g_params, z)
 
@@ -149,26 +143,39 @@ def g_loss(g_params, d_params, prng_key, batch_size):
     return loss
 
 
-@partial(jit, static_argnums=(5,))
-def train_step(i, prng_key, d_state, g_state, real_ims, batch_size):
+@partial(jit, static_argnums=(7,))
+def train_step(i, prng_key, d_state, g_state, d_opt, g_opt, d_apply, g_apply, real_ims, batch_size):
     prng1, prng2 = jax.random.split(prng_key, 2)
     d_params = d_opt['get_params'](d_state)
     g_params = g_opt['get_params'](g_state)
 
-    d_loss_value, d_grads = value_and_grad(d_loss)(d_params, g_params, prng1, batch_size, real_ims)
+    d_loss_value, d_grads = value_and_grad(d_loss)(d_params, g_params, d_apply, g_apply, prng1, batch_size, real_ims)
     d_state = d_opt['update'](i, d_grads, d_state)
 
-    g_loss_value, g_grads = value_and_grad(g_loss)(g_params, d_params, prng2, batch_size)
+    g_loss_value, g_grads = value_and_grad(g_loss)(g_params, d_params, g_apply, d_apply, prng2, batch_size)
     g_state = g_opt['update'](i, g_grads, g_state)
 
     return d_state, g_state, d_loss_value, g_loss_value
 
 
 def train(batch_size, num_iter, digit):
-    prng_key = jax.random.PRNGKey(0)
+    # getting model modules
+    d_init, d_apply = conv_discriminator()
+    g_init, g_apply = conv_generator()
+
+    # getting optimizer modules
+    (d_opt_init_fun, d_opt_update_fun, d_opt_get_params) = adam(d_lr_default, d_momentum_default, d_momentum2_default)
+    (g_opt_init_fun, g_opt_update_fun, g_opt_get_params) = adam(g_lr_default, g_momentum_default, g_momentum2_default)
+    d_opt = {'init': d_opt_init_fun, 'update': d_opt_update_fun, 'get_params': d_opt_get_params}
+    g_opt = {'init': g_opt_init_fun, 'update': g_opt_update_fun, 'get_params': g_opt_get_params}
+
+    # initializing model weights
+    prng_key = jax.random.PRNGKey(random_seed_default)
     prng1, prng2, prng = jax.random.split(prng_key, 3)
     d_output_shape, d_params = d_init(prng1, (batch_size, 32, 32, 1))
     g_output_shape, g_params = g_init(prng2, (batch_size, 100))
+
+    # initializing optimizer states
     d_state = d_opt['init'](d_params)
     g_state = g_opt['init'](g_params)
 
@@ -176,7 +183,6 @@ def train(batch_size, num_iter, digit):
     g_losses = []
 
     start_time = time.time()
-    prev_time = time.time()
     i = 0
     dataset = mnist_dataset(batch_size, digit=digit)
     while i < num_iter:
@@ -185,9 +191,6 @@ def train(batch_size, num_iter, digit):
             if i >= num_iter:
                 break
             if i % 100 == 0:
-                print(f"{i}/{num_iter} took {time.time()-prev_time}")
-                prev_time = time.time()
-
                 z = jax.random.normal(jax.random.PRNGKey(0), (1, 100))
                 fake = g_apply(g_opt["get_params"](g_state), z)
                 fake = fake.reshape((28, 28))
