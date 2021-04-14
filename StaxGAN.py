37abd4ea72b1e60035224c7302ad8f02c5052e06
Romina <rominaabadi1994@gmail.com>
2021-04-14 03:13:11 -0400

    cleaned and added options

--- StaxGAN.py
+++ StaxGAN.py
@@ -7,8 +7,9 @@ import argparse
 import time
 from functools import partial
 
-from Models import conv_generator_mnist, conv_discriminator
+from Models import conv_generator_mnist, conv_generator_cifar10, conv_discriminator
 from dataset_loader import get_NumpyLoader_mnist as mnist_dataset
+from dataset_loader import get_NumpyLoader_cifar10 as cifar10_dataset
 
 
 # ~~~~~~~~~~~~ helper functions ~~~~~~~~~~~~~~~~~~~~~~~~
@@ -34,6 +35,7 @@ def MSE(logits, desired_values):
 
 
 # ~~~~~~~~~~~ Stax GAN ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+dataset_default = 'mnist'
 
 d_lr_default = 0.0002
 d_momentum_default = 0.5
@@ -47,14 +49,32 @@ batch_size_default = 128
 num_iter_default = 2000
 digit_default = 0
 
+dataset_loaders = {'mnist': mnist_dataset, 'cifar10': cifar10_dataset}
+generators = {'mnist': conv_generator_mnist, 'cifar10': conv_generator_cifar10}
 
+
+dataset_loader = dataset_loaders[dataset_default]
 d_init, d_apply = conv_discriminator()
-g_init, g_apply = conv_generator_mnist()
-
+g_init, g_apply = generators[dataset_default]()
 (d_opt_init_fun, d_opt_update_fun, d_opt_get_params) = adam(d_lr_default, d_momentum_default, d_momentum2_default)
 (g_opt_init_fun, g_opt_update_fun, g_opt_get_params) = adam(g_lr_default, g_momentum_default, g_momentum2_default)
 d_opt = {'init': d_opt_init_fun, 'update': d_opt_update_fun, 'get_params': d_opt_get_params}
 g_opt = {'init': g_opt_init_fun, 'update': g_opt_update_fun, 'get_params': g_opt_get_params}
+
+
+def initialize(dataset,
+               d_lr=d_lr_default, d_momentum=d_momentum_default, d_momentum2=d_momentum2_default,
+               g_lr=g_lr_default, g_momentum=g_momentum_default, g_momentum2=g_momentum2_default,
+               ):
+    globals()['dataset_loader'] = dataset_loaders[dataset]
+
+    globals()['d_init'], globals()['d_apply'] = conv_discriminator()
+    globals()['g_init'], globals()['g_apply'] = generators[dataset]()
+
+    (d_opt_init_fun, d_opt_update_fun, d_opt_get_params) = adam(d_lr, d_momentum, d_momentum2)
+    (g_opt_init_fun, g_opt_update_fun, g_opt_get_params) = adam(g_lr, g_momentum, g_momentum2)
+    globals()['d_opt'] = {'init': d_opt_init_fun, 'update': d_opt_update_fun, 'get_params': d_opt_get_params}
+    globals()['g_opt'] = {'init': g_opt_init_fun, 'update': g_opt_update_fun, 'get_params': g_opt_get_params}
 
 
 @partial(jit, static_argnums=(3,))
@@ -98,7 +118,16 @@ def train_step(i, prng_key, d_state, g_state, real_ims, batch_size):
     return d_state, g_state, d_loss_value, g_loss_value
 
 
-def train(batch_size, num_iter, digit):
+def train(batch_size, num_iter, digit,
+          dataset=dataset_default,
+          d_lr=d_lr_default, d_momentum=d_momentum_default, d_momentum2=d_momentum2_default,
+          g_lr=g_lr_default, g_momentum=g_momentum_default, g_momentum2=g_momentum2_default,
+          ):
+    initialize(
+        dataset=dataset,
+        d_lr=d_lr, d_momentum=d_momentum, d_momentum2=d_momentum2,
+        g_lr=g_lr, g_momentum=g_momentum, g_momentum2=g_momentum2,
+    )
     prng_key = jax.random.PRNGKey(0)
     prng1, prng2, prng = jax.random.split(prng_key, 3)
     d_output_shape, d_params = d_init(prng1, (batch_size, 32, 32, 1))
@@ -112,14 +141,14 @@ def train(batch_size, num_iter, digit):
     start_time = time.time()
     prev_time = time.time()
     i = 0
-    dataset = mnist_dataset(batch_size, digit=digit)
+    dataset = dataset_loader(batch_size, digit=digit)
     while i < num_iter:
         epoch_start_time = time.time()
         for real_ims, _ in dataset:
             if i >= num_iter:
                 break
             if i % 100 == 0:
-                print(f"{i}/{num_iter} took {time.time()-prev_time}")
+                print(f"{i}/{num_iter} took {time.time() - prev_time}")
                 prev_time = time.time()
 
                 z = jax.random.normal(jax.random.PRNGKey(0), (1, 100))
@@ -129,12 +158,13 @@ def train(batch_size, num_iter, digit):
                 plt.show()
 
             prng, prng_to_use = jax.random.split(prng, 2)
-            d_state, g_state, d_loss_value, g_loss_value = train_step(i, prng_to_use, d_state, g_state, real_ims, batch_size)
+            d_state, g_state, d_loss_value, g_loss_value = train_step(i, prng_to_use, d_state, g_state, real_ims,
+                                                                      batch_size)
             d_losses.append(d_loss_value)
             g_losses.append(g_loss_value)
             i = i + 1
-        print(f'epoch finished in {time.time()-epoch_start_time}')
-    print(f'finished, took{time.time()-start_time}')
+        print(f'epoch finished in {time.time() - epoch_start_time}')
+    print(f'finished, took{time.time() - start_time}')
 
     return d_losses, g_losses, d_state, g_state
 
@@ -147,13 +177,27 @@ if __name__ == '__main__':
                         help="number of iterations")
     parser.add_argument("--digit", required=False, default=digit_default, type=int,
                         help="digit")
+    parser.add_argument("--dataset", required=False, default=dataset_default, type=str,
+                        choices={'mnist','cifar10'}, help="the dataset for training")
+    parser.add_argument("--d_lr", required=False, default=d_lr_default, type=jnp.float32,
+                        help="discriminator learning rate")
+    parser.add_argument("--d_momentum", required=False, default=d_momentum_default, type=jnp.float32,
+                        help="discriminator momentum")
+    parser.add_argument("--d_momentum2", required=False, default=d_momentum2_default, type=jnp.float32,
+                        help="discriminator second momentum")
+    parser.add_argument("--g_lr", required=False, default=g_lr_default, type=jnp.float32,
+                        help="generator learning rate")
+    parser.add_argument("--g_momentum", required=False, default=g_momentum_default, type=jnp.float32,
+                        help="generator momentum")
+    parser.add_argument("--g_momentum2", required=False, default=g_momentum2_default, type=jnp.float32,
+                        help="generator second momentum")
+
     args = vars(parser.parse_args())
     train(
         batch_size=args['batch_size'],
         num_iter=args['num_iter'],
-        digit=args['digit']
+        digit=args['digit'],
+        dataset=args['dataset'],
+        d_lr=args['d_lr'], d_momentum=args['d_momentum'], d_momentum2=args['d_momentum2'],
+        g_lr=args['g_lr'], g_momentum=args['g_momentum'], g_momentum2=args['g_momentum2'],
     )
-
-
-
-
